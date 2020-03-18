20c13d7dd7ebcd796a4e945a7ad6db30b7ae8916
Nicholas Patel <np4795@yahoo.com>
2020-03-18 00:13:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,48 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 1, 1), ("James Bond", 2, 2); 
+def select_books_titles_and_years_in_first_series_order_by_year
+  "SELECT books.title, books. year FROM books 
+    INNER JOIN series 
+      ON books.series_id = series.id 
+    GROUP BY books.year HAVING series.id = 1; "
+end
 
-INSERT INTO books (title, year, series_id) VALUES ("Harry Potter", 2004, 1), ("A Clash of Kings", 1998, 1), ("A Storm of Swords", 2000, 1), ("First Book", 2002, 2), ("Second Book", 2003, 2), ("Third Book", 2005, 2); 
+def select_name_and_motto_of_char_with_longest_motto
+  "SELECT characters.name, characters.motto FROM characters
+    ORDER BY characters.motto ASC LIMIT 1;"
+end
 
-INSERT INTO characters (name, motto, species, author_id) VALUES ("joe", "hey there", "human", "1"), ("joe", "hey there", "human", "1"), ("joe", "hey there", "human", "1"),("joe", "hey there", "human", "1");
-INSERT INTO characters (name, motto, species, author_id) VALUES ("joe", "hey there", "human", "1"), ("joe", 
+
+def select_value_and_count_of_most_prolific_species
+  "SELECT species, COUNT(species) FROM characters
+    GROUP BY species  
+    ORDER BY COUNT(species) DESC 
+    LIMIT 1;"
+end
+
+def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name FROM series
+  INNER JOIN authors 
+    ON series.author_id = authors.id
+  INNER JOIN subgenres 
+    ON series.subgenre_id = subgenres.id;" 
+end
+
+def select_series_title_with_most_human_characters
+  "SELECT series.title FROM series 
+    INNER JOIN books 
+      ON series.id = books.series_id 
+    INNER JOIN character_books 
+      ON  character_books.book_id = books.id
+    INNER JOIN characters
+      ON characters.id = character_books.character_id WHERE characters.species = 'human' 
+    GROUP BY series.title 
+    ORDER BY COUNT(*) DESC LIMIT 1;"
+end
+
+def select_character_names_and_number_of_books_they_are_in
+  "SELECT characters.name, COUNT(character_books.book_id) AS book_count 
+    FROM character_books 
+    JOIN characters 
+      ON character_books.character_id = characters.id 
+    GROUP BY characters.name
+    ORDER BY book_count DESC, characters.name;"
+end
