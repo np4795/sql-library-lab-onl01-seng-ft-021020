b418fb99d23a4d74f74bae92dec90eed563a8cb4
Nicholas Patel <np4795@yahoo.com>
2020-03-18 00:13:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,6 @@
-def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
-end
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 1, 1), ("James Bond", 2, 2); 
 
-def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
-end
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter", 2004, 1), ("A Clash of Kings", 1998, 1), ("A Storm of Swords", 2000, 1), ("First Book", 2002, 2), ("Second Book", 2003, 2), ("Third Book", 2005, 2); 
 
-
-def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
-end
-
-def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
-end
-
-def select_series_title_with_most_human_characters
-  "Write your SQL query here"
-end
-
-def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
-end
+INSERT INTO characters (name, motto, species, author_id) VALUES ("joe", "hey there", "human", "1"), ("joe", "hey there", "human", "1"), ("joe", "hey there", "human", "1"),("joe", "hey there", "human", "1");
+INSERT INTO characters (name, motto, species, author_id) VALUES ("joe", "hey there", "human", "1"), ("joe", 
